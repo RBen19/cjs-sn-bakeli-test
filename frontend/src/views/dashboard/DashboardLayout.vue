81f574a9bbe7bcc5fa85cb7faabeea84807b8abd
Rosinard Beni <bonmerciel@gmail.com>
2026-02-20 10:49:18 +0000

    feat(frontend): add dashboard layout with sidebar

---
+++ frontend/src/views/dashboard/DashboardLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Sidebar from '../../components/dashboard/Sidebar.vue'
+</script>
+
+<template>
+  <div class="flex min-h-screen bg-gray-50">
+    <Sidebar />
+    <div class="flex-1 min-w-0 overflow-hidden">
+      <RouterView />
+    </div>
+  </div>
+</template>
