443ed82a74e54872762694ade36017222ef37b51
Rosinard Beni <bonmerciel@gmail.com>
2026-02-20 10:48:59 +0000

    feat(frontend): add landing page view composing all sections

---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NavBar from '../components/landing/NavBar.vue'
+import HeroSection from '../components/landing/HeroSection.vue'
+import FeaturesSection from '../components/landing/FeaturesSection.vue'
+import AboutSection from '../components/landing/AboutSection.vue'
+import CTASection from '../components/landing/CTASection.vue'
+import FooterSection from '../components/landing/FooterSection.vue'
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <NavBar />
+    <main>
+      <HeroSection />
+      <FeaturesSection />
+      <AboutSection />
+      <CTASection />
+    </main>
+    <FooterSection />
+  </div>
+</template>
